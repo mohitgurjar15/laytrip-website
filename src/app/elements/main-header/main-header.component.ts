3680cc9a0524bd72be428e418d61e4c2545c8ab8
suresh555 <suresh@itoneclick.com>
2020-07-28 13:07:59 +0530

    Create session for lanunage & currency

--- src/app/elements/main-header/main-header.component.ts
+++ src/app/elements/main-header/main-header.component.ts
@@ -14,20 +14,57 @@ export class MainHeaderComponent implements OnInit {
 
     langunages:Langunage[]=[];
     selectedLanunage:Langunage={ id : 0, name : '', iso_1Code:'', iso_2Code:'',active:false};
+    isLanunageSet:boolean=false;
 
     currencies:Currency[]=[];
     selectedCurrency:Currency={ id:0, country:'', code:'',symbol:'', status:false }
+    isCurrencySet:boolean=false;
+
 
     s3BucketUrl = environment.s3BucketUrl;
     constructor(
       private genericService:GenericService,
       public translate: TranslateService
     ) { 
-      translate.setDefaultLang('en');
+        let _langunage = localStorage.getItem('_lang');
+        let _currency = localStorage.getItem('_curr');
+        if(_langunage){
+
+          try{
+
+            let _lang = JSON.parse(_langunage);
+            this.selectedLanunage = _lang;
+            translate.setDefaultLang(this.selectedLanunage.iso_1Code);
+            this.isLanunageSet=true;
+          }
+          catch(error){
+            this.isLanunageSet=false;
+            translate.setDefaultLang('en');
+          }
+          
+        }
+        else{
+
+          translate.setDefaultLang('en');
+        }
+
+        if(_currency){
+
+          try{
+            let _curr = JSON.parse(_currency);
+            this.selectedCurrency = _curr;
+            this.isCurrencySet=true;
+          }
+          catch(error){
+            this.isCurrencySet=false;
+          }
+        }
+
     }
 
     ngOnInit(): void {
-        
+      
+      
       this.getLangunages();
       this.getCurrencies();
     }
@@ -38,11 +75,11 @@ export class MainHeaderComponent implements OnInit {
      */
     changeLangunage(langunage:Langunage){
 
-
-      if(JSON.stringify(langunage)!=JSON.stringify(this.selectedLanunage)){
-          this.selectedLanunage = langunage;
-          this.translate.use(langunage.iso_1Code);
-      }
+        if(JSON.stringify(langunage)!=JSON.stringify(this.selectedLanunage)){
+            this.selectedLanunage = langunage;
+            localStorage.setItem("_lang", JSON.stringify(langunage))
+            this.translate.use(langunage.iso_1Code);
+        }
     }
 
     /**
@@ -52,8 +89,11 @@ export class MainHeaderComponent implements OnInit {
       this.genericService.getAllLangunage().subscribe(
         (response:LangunageModel)=>{
           this.langunages = response.data.filter(lang => lang.active==true);
-          console.log(this.langunages)
-          this.selectedLanunage=this.langunages[0];
+          if(!this.isLanunageSet){
+            this.isLanunageSet=true;
+            this.selectedLanunage=this.langunages[0];
+            localStorage.setItem("_lang", JSON.stringify(this.langunages[0]))
+          }
         },
         (error)=>{
 
@@ -70,8 +110,12 @@ export class MainHeaderComponent implements OnInit {
         (response:CurrencyModel)=>{
 
           this.currencies = response.data.filter(currency=>currency.status==true);
-          this.selectedCurrency=this.currencies[0];
-          console.log(this.selectedCurrency)
+          if(!this.isCurrencySet){
+
+            this.isCurrencySet=true;
+            this.selectedCurrency=this.currencies[0];
+            localStorage.setItem("_curr", JSON.stringify(this.currencies[0]))
+          }
         },
         (error)=>{
 
@@ -82,6 +126,7 @@ export class MainHeaderComponent implements OnInit {
     changeCurrency(currency:Currency){
       if(JSON.stringify(currency)!=JSON.stringify(this.selectedCurrency)){
         this.selectedCurrency = currency;
+        localStorage.setItem("_curr", JSON.stringify(currency))
       }
     }
 }
