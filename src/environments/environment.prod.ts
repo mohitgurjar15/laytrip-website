1e0f3e75287fb4c74e135850c63c59513477a2fb
suresh555 <suresh@itoneclick.com>
2020-07-30 15:52:40 +0530

    CI/CD testing

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl:'http://oneclickitmarketing.co.in:4045/',
+  apiUrl:'http://api.laytrip.com/',
   s3BucketUrl:'http://d2q1prebf1m2s9.cloudfront.net/'
 };
