798313c13f47c356b7ddb2fc52e29ea218784dd2
suresh555 <suresh@itoneclick.com>
2020-07-30 18:58:33 +0530

    testing with umag

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl:'http://api.laytrip.com/',
+  apiUrl:'http://oneclickitmarketing.co.in:4040/',
   s3BucketUrl:'http://d2q1prebf1m2s9.cloudfront.net/'
 };
