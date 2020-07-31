d7d944a981b19df37bdb741c46cb8e183be57d44
suresh555 <suresh@itoneclick.com>
2020-07-31 09:38:28 +0530

    Package change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl:'http://oneclickitmarketing.co.in:4040/',
+  apiUrl:'http://api.laytrip.com/',
   s3BucketUrl:'http://d2q1prebf1m2s9.cloudfront.net/'
 };
