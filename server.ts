ca100a4456cca822bf8d44471df6cb22526d6661
suresh555 <suresh@itoneclick.com>
2020-08-16 17:13:21 +0530

    Changes

--- server.ts
+++ server.ts
@@ -7,6 +7,9 @@ import {provideModuleMap} from '@nguniversal/module-map-ngfactory-loader';
 
 import * as express from 'express';
 import {join} from 'path';
+import 'localstorage-polyfill'
+
+global['localStorage'] = localStorage;
 
 // Faster server renders w/ Prod mode (dev mode never needed)
 enableProdMode();
