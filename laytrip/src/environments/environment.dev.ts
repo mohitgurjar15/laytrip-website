2eea685d234a7f0b569de9651f6a172f3b24683f
mohit <mohit@itoneclick.com>
2020-08-22 14:06:10 +0530

    changes by:Mohit for modals

--- laytrip/src/environments/environment.dev.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  name:'dev',
-  apiUrl:'https://staging.laytrip.com:4047/',
- s3BucketUrl:'http://d2q1prebf1m2s9.cloudfront.net/'
-};
--- laytrip/src/environments/environment.suresh.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  name:'suresh',
-  apiUrl:'https://staging.laytrip.com:4047/',
-  s3BucketUrl:'http://d2q1prebf1m2s9.cloudfront.net/'
-};
