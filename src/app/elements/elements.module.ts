046a6f2b5f5963453482b0dbabb0e5fc9252da52
mohit <mohit@itoneclick.com>
2020-08-22 14:15:45 +0530

    changes by:Mohit for modals

--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -3,14 +3,14 @@ import { CommonModule } from '@angular/common';
 import { SecondaryFooterComponent } from './secondary-footer/secondary-footer.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { PagesModule } from '../pages/pages.module';
-import { SigninComponent } from 'laytrip/src/app/pages/user/signin/signin.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { SocialLoginComponent } from 'laytrip/src/app/pages/user/social-login/social-login.component';
+import { SigninComponent } from '../pages/user/signin/signin.component';
+import { SocialLoginComponent } from '../pages/user/social-login/social-login.component';
 
 
 
 @NgModule({
-  declarations: [SecondaryFooterComponent,SigninComponent,SocialLoginComponent],
+  declarations: [SecondaryFooterComponent],
   imports: [
     CommonModule,
     TranslateModule,
@@ -20,6 +20,6 @@ import { SocialLoginComponent } from 'laytrip/src/app/pages/user/social-login/so
   ],
   exports:[
   ],
-  entryComponents:[SigninComponent,SocialLoginComponent]
+  entryComponents:[]
 })
 export class ElementsModule { }
--- src/app/pages/user/user.module.ts
+++ src/app/pages/user/user.module.ts
@@ -2,11 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignupComponent } from './signup/signup.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { VerifyOtpComponent } from './verify-otp/verify-otp.component';
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [VerifyOtpComponent],
+  declarations: [],
   imports: [
     CommonModule,
     ReactiveFormsModule,
