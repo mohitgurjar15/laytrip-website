0ab1eb385355d2a5d438701b4096ca10856a258c
suresh555 <suresh@itoneclick.com>
2020-09-24 20:52:40 +0530

    fix bugs & UI

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -90,8 +90,31 @@ export class HomeComponent implements OnInit {
       dots: false,
       infinite: true,
       slidesToShow: 3,
-      slidesToScroll: 1
-    });
+      slidesToScroll: 1,
+          responsive: [
+              {
+                  breakpoint: 1200,
+                  settings: {
+                      slidesToShow: 3,
+                      slidesToScroll: 1
+                  }
+              },
+              {
+                  breakpoint: 992,
+                  settings: {
+                      slidesToShow: 2,
+                      slidesToScroll: 1
+                  }
+              },
+              {
+                  breakpoint: 600,
+                  settings: {
+                      slidesToShow: 1,
+                      slidesToScroll: 1
+                  }
+              }
+          ]
+      });
 
     // Start Featured List Js
     $(".deals_slid").slick({
