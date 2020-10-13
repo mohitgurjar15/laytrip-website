9bf17e7a3d569a6653c44f4d94d85d697ecfffd2
varshil doshi <varshil@itoneclick.com>
2020-10-13 17:05:26 +0530

    api call after apple login

--- src/app/pages/user/social-login/social-login.component.ts
+++ src/app/pages/user/social-login/social-login.component.ts
@@ -8,7 +8,8 @@ import { Location } from '@angular/common';
 import { SocialAuthService, SocialUser } from 'angularx-social-login';
 import { AppleLoginProvider } from './apple.provider';
 declare var $: any;
-user: SocialUser;
+import * as jwt_decode from "jwt-decode";
+
 @Component({
   selector: 'app-social-login',
   templateUrl: './social-login.component.html',
@@ -19,7 +20,7 @@ export class SocialLoginComponent implements OnInit {
   s3BucketUrl = environment.s3BucketUrl;
   apiError: string = '';
   test: boolean = false;
-  user: SocialUser;
+  userInfo: SocialUser;
 
   constructor(
     private userService: UserService,
@@ -36,9 +37,25 @@ export class SocialLoginComponent implements OnInit {
     this.loadGoogleSdk();
     this.loadFacebookSdk();
     this.authService.authState.subscribe((user) => {
-      console.log('USER:::::', user);
-      this.user = user;
-      // this.loggedIn = (user != null);
+      if (user) {
+        console.log('USER:::::', user);
+        let objApple = jwt_decode(user.idToken);
+        console.log(objApple);
+        let json_data = {
+          "account_type": 1,
+          "name": '',
+          "email": objApple.email,
+          "social_account_id": user.authorizationCode,
+          "device_type": 1,
+          "device_model": "RNE-L22",
+          "device_token": "123abc#$%456",
+          "app_version": "1.0",
+          "os_version": "7.0"
+        };
+        // this.userService.socialLogin(json_data).subscribe((data: any) => {
+        //   console.log(data);
+        // });
+      }
     });
   }
 
