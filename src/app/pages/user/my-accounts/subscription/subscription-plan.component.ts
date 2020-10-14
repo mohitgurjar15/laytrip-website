fb149699c9bad907730f9576b0dde03069c1109e
varshil doshi <varshil@itoneclick.com>
2020-10-14 16:51:52 +0530

    subscription loader

--- src/app/pages/user/my-accounts/subscription/subscription-plan.component.ts
+++ src/app/pages/user/my-accounts/subscription/subscription-plan.component.ts
@@ -12,18 +12,21 @@ export class SubscriptionPlanComponent implements OnInit {
   s3BucketUrl = environment.s3BucketUrl;
   subscriptionList;
   currency;
+  loading = false;
 
   constructor(
     private userService: UserService,
   ) { }
 
   ngOnInit() {
+    this.loading = true;
     let _currency = localStorage.getItem('_curr');
     this.currency = JSON.parse(_currency);
     this.userService.getSubscriptionList().subscribe((res: any) => {
       // console.log(res.data);
       if (res && res.data) {
         this.subscriptionList = res.data;
+        this.loading = false;
       }
     });
   }
