8c729459a4602313c78d9bb1ef0fcf72468c3252
suresh555 <suresh@itoneclick.com>
2020-11-06 12:08:01 +0530

    changes

--- src/app/elements/main-header/main-header.component.ts
+++ src/app/elements/main-header/main-header.component.ts
@@ -246,15 +246,32 @@ export class MainHeaderComponent implements OnInit, DoCheck {
       let location:any = this.cookieService.get('__loc');      
       if(typeof location=='undefined'){
         this.genericService.getUserLocationInfo().subscribe((res:any)=>{
+          console.log("location",res)
           this.cookieService.put('__loc',JSON.stringify(res), { expires : new Date(moment().add('7',"days").format())})
+          this.redirectToDRsite(res)
         },(error)=>{
     
         })
+      }
+      else{
+        console.log("window.location.origin",window.location.origin)
+        location = JSON.parse(location);
+        this.redirectToDRsite(location)
+        
       }
     }
     catch(e){
 
     }
     
+  }
+
+  redirectToDRsite(location){
+    console.log("Location",location)
+    if(location.country.iso2=='PL'){
+      if(window.location.origin=='https://staging.laytrip.com' || window.location.origin=='http://staging.laytrip.com' || window.location.origin=='http://localhost:4200' ){
+        window.location.href='https://www.google.com/'
+      }
+    }
   }
 }
