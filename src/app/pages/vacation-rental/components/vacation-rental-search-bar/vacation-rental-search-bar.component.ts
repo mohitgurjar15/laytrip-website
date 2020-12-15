9654a6c3e7512ddbc3a6d8e371aef2caa5020313
keyur <keyur@itoneclick.com>
2020-12-15 17:52:20 +0530

    commit issue changes

--- src/app/pages/vacation-rental/components/vacation-rental-search-bar/vacation-rental-search-bar.component.ts
+++ src/app/pages/vacation-rental/components/vacation-rental-search-bar/vacation-rental-search-bar.component.ts
@@ -151,8 +151,8 @@ export class VacationRentalSearchBarComponent implements OnInit {
     }
   }
 
-   selectEvent(event, index) {
-     console.log(event,index);
+   selectEvent(event) {
+     console.log(event);
      if (!event) {
       this.placeHolder = this.placeHolder;
       this.defaultSelected = this.defaultSelected;
