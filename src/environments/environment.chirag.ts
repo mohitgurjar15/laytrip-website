abf8aba54c856988d4b0fc8293934ce71284906f
Chirag Khatri <chirag@itoneclick.com>
2020-12-31 16:55:46 +0530

    added new config file

---
+++ src/environments/environment.chirag.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production: false,
+    name: 'chirag',
+    apiUrl: 'http://staging.laytrip.com:4040/',
+    s3BucketUrl: 'http://laytrip-ui.local.com/',
+    fb_api_key: '402941427334423',
+    google_client_id: '154754991565-9lo2g91remkuefocr7q2sb92g24jntba.apps.googleusercontent.com',
+    siteUrl: 'https://staging.laytrip.com',
+    VAPID_PUBLIC_KEY:'BL6lEBuIL5QndQkV6pP-r1za33NJQ0u9fj2SWplSfk3ZmKj5i7Kcyq9C1simRWRxfgHXQHF_8zFDYO8jv6ljF68'
+};
