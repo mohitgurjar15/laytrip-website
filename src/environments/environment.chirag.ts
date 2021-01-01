037c889f1fc39cbc203cb3673d92b2318e799fc1
Chirag Khatri <chirag@itoneclick.com>
2021-01-01 15:39:00 +0530

    changes

--- src/environments/environment.chirag.ts
+++ src/environments/environment.chirag.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     name: 'chirag',
-    apiUrl: 'http://staging.laytrip.com:4040/',
+    apiUrl: 'http://localhost:4040/',
     s3BucketUrl: 'http://laytrip-ui.local.com/',
     fb_api_key: '402941427334423',
     google_client_id: '154754991565-9lo2g91remkuefocr7q2sb92g24jntba.apps.googleusercontent.com',
