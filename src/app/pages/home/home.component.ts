f762921d77f37119ef516b2e6ad0232d95a31967
chintan_rao <chintan@itoneclick.com>
2021-01-06 15:32:46 +0530

    Home page bg changes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -110,7 +110,7 @@ export class HomeComponent implements OnInit {
     document.getElementById('home_banner').style.width = '100%';
     document.getElementById('home_banner').style.paddingBottom = '180px';
     if (tabName === 'flight') {
-      document.getElementById('home_banner').style.background = "url(" + this.s3BucketUrl + "assets/images/flight-tab-bg.svg) no-repeat";
+      document.getElementById('home_banner').style.background = "url(" + this.s3BucketUrl + "assets/images/flight-tab-new-bg.svg) no-repeat";
       document.getElementById('home_banner').style.backgroundRepeat = 'no-repeat';
       document.getElementById('home_banner').style.backgroundSize = 'cover';
       // if (document.getElementById('login_btn')) {
