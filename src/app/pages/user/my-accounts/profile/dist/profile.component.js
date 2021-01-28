b340b653173ba4cdd65abb7cdcb44e545e9341cc
mohit <mohit@itoneclick.com>
2021-01-28 18:48:27 +0530

    changes by:Mohit modal completedusus

--- src/app/pages/user/my-accounts/profile/dist/profile.component.js
+++ src/app/pages/user/my-accounts/profile/dist/profile.component.js
@@ -56,9 +56,10 @@ var ProfileComponent = /** @class */ (function () {
             { key: 'M', name: 'Male' },
             { key: 'F', name: 'Female' },
         ];
-        this.startYear = 1971;
     }
     ProfileComponent.prototype.ngOnInit = function () {
+        this.startYear = "1971:" + this.dobMinDate.getFullYear();
+        console.log(this.startYear);
         window.scroll(0, 0);
         this.getCountry();
         this.getLanguages();
--- src/app/pages/user/my-accounts/profile/profile.component.ts
+++ src/app/pages/user/my-accounts/profile/profile.component.ts
@@ -57,7 +57,7 @@ export class ProfileComponent implements OnInit {
     { key: 'M', name: 'Male' },
     { key: 'F', name: 'Female' },
   ];
-  startYear = 1971;
+  startYear;
   constructor(
     private formBuilder: FormBuilder,
     private userService : UserService,
@@ -70,6 +70,8 @@ export class ProfileComponent implements OnInit {
     ) {}
  
   ngOnInit() {
+    this.startYear = "1971:"+this.dobMinDate.getFullYear();
+    console.log(this.startYear)
     window.scroll(0,0);
     this.getCountry();
     this.getLanguages();
