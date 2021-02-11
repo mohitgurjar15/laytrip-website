f624cccbd386a74f16f429c7332137d7f8408530
mohit <mohit@itoneclick.com>
2021-02-11 22:38:49 +0530

    changes by:Mohit for work in myaccount / google login

--- src/app/pages/user/my-accounts/bookings/list-bookings.component.ts
+++ src/app/pages/user/my-accounts/bookings/list-bookings.component.ts
@@ -3,8 +3,8 @@ import { UserService } from '../../../../services/user.service';
 import { CommonFunction } from '../../../../_helpers/common-function';
 import { environment } from '../../../../../environments/environment';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { FlightService } from 'src/app/services/flight.service';
-import { AccountService } from 'src/app/services/account.service';
+import { FlightService } from '../../../../services/flight.service';
+import { AccountService } from '../../../../services/account.service';
 
 @Component({
   selector: 'app-list-bookings',
@@ -15,17 +15,18 @@ export class ListBookingsComponent implements OnInit {
   s3BucketUrl = environment.s3BucketUrl;
   upComingloading = false;
   upComingbookings=[];
+  completeLoading = false;
+  completeBookings=[];
 
   constructor(
     private userService: UserService,
     private accountService: AccountService,
     private commonFunction: CommonFunction,
-    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit() {
-    // this.loading = true;
     this.getIncomplteBooking();
+    this.getComplteBooking();
   }
 
   getIncomplteBooking(){
@@ -33,10 +34,22 @@ export class ListBookingsComponent implements OnInit {
     this.accountService.getIncomplteBooking().subscribe((res: any) => {
       this.upComingbookings=res.data;
       this.upComingloading=false;
-      console.log(res)
    }, err => {
     this.upComingloading=false;
     this.upComingbookings=[];
+   }); 
+  }
+
+  getComplteBooking(){
+    this.completeLoading=true;
+    this.accountService.getComplteBooking().subscribe((res: any) => {
+      console.log(res)
+
+      this.completeBookings=res.data;
+      this.completeLoading=false;
+   }, err => {
+    this.completeLoading=false;
+    this.completeBookings=[];
    }); 
   }
 }
