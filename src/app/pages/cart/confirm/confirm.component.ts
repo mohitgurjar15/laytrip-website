e1aa6290fd8b37ee28bf02d5613609d65020f65c
varshil doshi <varshil@itoneclick.com>
2021-02-12 17:54:59 +0530

    merge conflict solved

--- src/app/pages/cart/confirm/confirm.component.ts
+++ src/app/pages/cart/confirm/confirm.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Renderer2 } from '@angular/core';
-import { environment } from 'src/environments/environment';
+import { environment } from '../../../../environments/environment';
 declare var $: any;
 
 @Component({
--- src/app/pages/user/my-accounts/bookings/cart-price-summary/cart-price-summary.component.ts
+++ src/app/pages/user/my-accounts/bookings/cart-price-summary/cart-price-summary.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
-import { CommonFunction } from 'src/app/_helpers/common-function';
+import { CommonFunction } from '../../../../../_helpers/common-function';
 
 @Component({
   selector: 'app-cart-price-summary',
@@ -8,7 +8,7 @@ import { CommonFunction } from 'src/app/_helpers/common-function';
 })
 export class CartPriceSummaryComponent implements OnInit {
 
-  @Input() cartItem = {};
+  @Input() cartItem: any = {};
 
   constructor(public commonFunction:CommonFunction) { }
 
