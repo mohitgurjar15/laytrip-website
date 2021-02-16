60b8a1e6be754593e375a87759adbfa8382b7e3a
suresh555 <suresh@itoneclick.com>
2021-02-16 17:12:04 +0530

    changes

--- src/app/pages/user/my-accounts/bookings/flights/flights.component.ts
+++ src/app/pages/user/my-accounts/bookings/flights/flights.component.ts
@@ -25,8 +25,6 @@ export class FlightsComponent implements OnInit {
   ngOnChanges(changes: SimpleChanges) {
     if(typeof changes['cartItem'].currentValue!='undefined'){
       this.cartItem=changes['cartItem'].currentValue;
-      console.log("cartItem=====>",this.cartItem)
-
     }
   }
 }
