1a92b2216ca17920fc54a92434edaa396d26ebdc
varshil doshi <varshil@itoneclick.com>
2021-03-23 18:26:44 +0530

    change

--- src/app/_helpers/phone-masking.helper.ts
+++ src/app/_helpers/phone-masking.helper.ts
@@ -1,55 +1,87 @@
 export const PHONE_NUMBER_MASK = {
     '+93': {
-        format: '000 000 000 0',
-        length: 10,
+        format: '000 000 000 000 0',
+        length: 13,
     },
     '+355': {
-        format: '000 000 000 0',
-        length: 10,
+        format: '000 000 000 000 0',
+        length: 13,
     },
     '+213': {
-        format: '000 000 000 0',
-        length: 10,
+        format: '000 000 000 000 0',
+        length: 13,
     },
     '+376': {
-        format: '000 000 000 0',
-        length: 10,
+        format: '000 000 000 000 0',
+        length: 13,
     },
     '+244': {
-        format: '000 000 000 0',
-        length: 10,
+        format: '000 000 000 000',
+        length: 12,
     },
     '+1268': {
-        format: '000 000 000 0',
-        length: 10,
+        format: '000 000 000 000',
+        length: 12,
     },
     '+54': {
         format: '(00) 00000000',
         length: 10,
     },
     '+374': {
-        format: '000 000 000 0',
-        length: 10,
+        format: '000 000 000 000',
+        length: 12,
     },
     '+297': {
-        format: '000 000 000 0',
-        length: 10,
+        format: '000 000 000 000',
+        length: 12,
     },
     '+61': {
-        format: '(00) 00000000',
+        format: '(00) 0000 0000',
         length: 10,
     },
     '+43': {
-        format: '000 000 000 0',
-        length: 10,
+        format: '000 000 000 000 0',
+        length: 13,
     },
     '+994': {
-        format: '000 000 000 0',
-        length: 10,
+        format: '000 000 000 000 0',
+        length: 13,
     },
     '+1242': {
-        format: '000 000 000 0',
-        length: 10,
+        format: '000 000 000 00',
+        length: 11,
+    },
+    '+973': {
+        format: '000 000 000 000',
+        length: 12,
+    },
+    '+880': {
+        format: '000 000 000 000',
+        length: 12,
+    },
+    '+1246': {
+        format: '000 000 000 000',
+        length: 12,
+    },
+    '+375': {
+        format: '(00) 000 00 00',
+        length: 9,
+    },
+    '+32': {
+        format: '000 00 00 00',
+        length: 9,
+    },
+    '+501': {
+        format: '000 000 000 000',
+        length: 12,
+    },
+    '+229': {
+        format: '000 000 000 000',
+        length: 12,
+    },
+    '+975': {
+        format: '000 000 000 000',
+        length: 12,
     },
     '+1': {
         format: '(000) 000-0000',
