b5b31437484067a84278f4dddc6ee672d3bb55e8
suresh555 <suresh@itoneclick.com>
2021-04-05 20:04:55 +0530

    changes

--- src/app/pages/flight/components/flight-price-slider/flight-price-slider.component.ts
+++ src/app/pages/flight/components/flight-price-slider/flight-price-slider.component.ts
@@ -22,18 +22,39 @@ export class FlightPriceSliderComponent implements OnInit {
   adult:number;
   child:number;
   infant:number;
-
-  slides = [
-    {img: "http://placehold.it/350x150/000000"},
-    {img: "http://placehold.it/350x150/111111"},
-    {img: "http://placehold.it/350x150/333333"},
-    {img: "http://placehold.it/350x150/666666"},
-    {img: "http://placehold.it/350x150/000000"},
-    {img: "http://placehold.it/350x150/111111"},
-    {img: "http://placehold.it/350x150/333333"},
-    {img: "http://placehold.it/350x150/666666"}
-  ];
-  slideConfig = {"slidesToShow": 7, "slidesToScroll": 1};
+  
+  slideConfig = {
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 7,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 7,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  }
 
   @Input() flexibleLoading:boolean=false;
   @Input() dates=[];
@@ -58,69 +79,15 @@ export class FlightPriceSliderComponent implements OnInit {
   }
 
   ngOnInit() {
-   // this.loadJquery();
   }
   breakpoint(e) {
     console.log('breakpoint', e);
   }
  
 
-  loadJquery() {
-    // Start Flight Price By Day slider js
-
-    if(this.dates.length>0){
-      let count = this.dates.length;
-      $('.price_day_slider').slick({
-        dots: false,
-        infinite: true,
-        speed: 300,
-        slidesToShow: count>=7?7:count,
-        slidesToScroll: 1,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: count>=6?6:count,
-              slidesToScroll: 1,
-              infinite: true,
-              dots: false
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: count>=3?3:count,
-              slidesToScroll: 1
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: count>=1?3:count,
-              slidesToScroll: 1
-            }
-          }
-        ]
-      });
-      // Close Flight Price By Day slider js
-    }
-    
-  }
-
   ngOnChanges(changes: SimpleChanges) {
    
     if(changes['dates'].currentValue.length){
-    //  setTimeout(()=>{this.loadJquery();},100)
-      /* if(this.trip == 'oneway'){
-        this.flipDates(this.dates)
-      }
-      else{
-        if(window.screen.width<=600){
-          let targetIndex=2;
-          this.dates = this.arrayRotate(this.dates,targetIndex);
-          console.log("this.dates",this.dates)
-        }
-      } */
       this.flipDates(this.dates)
     }
   }
@@ -131,8 +98,6 @@ export class FlightPriceSliderComponent implements OnInit {
     let targetIndex = 3;
     if(window.screen.width<=600){
       targetIndex=1;
-      /* targetIndex=dates.length-sourceIndex+1;
-      this.dates = this.rotateArray1(dates,targetIndex) */
     }
     let startIndex=sourceIndex-targetIndex;
 
@@ -145,38 +110,7 @@ export class FlightPriceSliderComponent implements OnInit {
     }
 
     this.dates = result;
-    /* else{
-      this.dates = this.arrayRotate(this.dates,targetIndex);
-    } */
-    /* if(targetIndex > sourceIndex){
-
-      targetIndex=5;
-      for(let i=targetIndex; i < this.dates.length; i++){
-        result.push(this.dates[i])
-      }
-      for(let i=0; i < targetIndex; i++){
-        result.push(this.dates[i])
-      }
-
-    }
-    else{
-
-      for(let i=targetIndex; i <= sourceIndex; i++){
-        
-        result.push(this.dates[i])
-      }
-      
-      for(let i=sourceIndex+1; i < this.dates.length; i++){
-        result.push(this.dates[i])
-      }
-      
-      for(let i=0; i < targetIndex; i++){
-        result.push(this.dates[i])
-      }
-
-      
-    }
-    this.dates = result; */
+    
   }
 
   rotateArray1(dates, k) {
