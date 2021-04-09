4d418eb84565483796866ff4bfa3ca58cb2eb057
mohit <mohit@itoneclick.com>
2021-04-09 15:48:41 +0530

    changes by:Mohit for error

---
+++ src/app/components/laytrip-loader/dist/laytrip-loader.component.js
@@ -0,0 +1,51 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+exports.__esModule = true;
+exports.LaytripLoaderComponent = void 0;
+var core_1 = require("@angular/core");
+var LaytripLoaderComponent = /** @class */ (function () {
+    function LaytripLoaderComponent(spinner) {
+        this.spinner = spinner;
+    }
+    LaytripLoaderComponent.prototype.ngOnInit = function () {
+        var lottie_path = '';
+        console.log(this.module, this.loading);
+        if (this.module == 'flight') {
+            lottie_path = 'assets/lottie-json/flight/data.json';
+        }
+        else if (this.module == 'hotel') {
+            lottie_path = 'assets/lottie-json/hotel/data.json';
+        }
+        else {
+            lottie_path = 'assets/lottie-json/flight/data.json';
+        }
+        this.lottieConfig = {
+            path: lottie_path,
+            autoplay: true,
+            loop: true
+        };
+    };
+    LaytripLoaderComponent.prototype.ngOnChanges = function (changes) {
+        console.log(changes);
+    };
+    __decorate([
+        core_1.Input()
+    ], LaytripLoaderComponent.prototype, "loading");
+    __decorate([
+        core_1.Input()
+    ], LaytripLoaderComponent.prototype, "module");
+    LaytripLoaderComponent = __decorate([
+        core_1.Component({
+            selector: 'app-laytrip-loader',
+            templateUrl: './laytrip-loader.component.html',
+            styleUrls: ['./laytrip-loader.component.scss']
+        })
+    ], LaytripLoaderComponent);
+    return LaytripLoaderComponent;
+}());
+exports.LaytripLoaderComponent = LaytripLoaderComponent;
--- src/app/components/laytrip-loader/laytrip-loader.component.ts
+++ src/app/components/laytrip-loader/laytrip-loader.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
@@ -10,16 +10,30 @@ export class LaytripLoaderComponent implements OnInit {
 
   public lottieConfig: Object;
   @Input() loading;
+  @Input() module;
 
   constructor(
     private spinner: NgxSpinnerService
   ) { }
 
   ngOnInit() {
+    var lottie_path = '';
+    console.log(this.module,this.loading)
+    if(this.module == 'flight'){
+      lottie_path = 'assets/lottie-json/flight/data.json';   
+    } else if(this.module == 'hotel'){
+      lottie_path = 'assets/lottie-json/hotel/data.json'; 
+    } else {
+      lottie_path = 'assets/lottie-json/flight/data.json';
+    }
     this.lottieConfig = {
-      path: 'assets/data.json',
+      path: lottie_path,
       autoplay: true,
       loop: true
     };
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(changes)
   }
 }
