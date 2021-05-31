26de46611c7223479364d1325480e2c37a4001a1
mohit <mohit@itoneclick.com>
2021-05-31 18:35:21 +0530

    widget landing page taskull

--- src/app/pages/home/flight-search-widget/dist/flight-search-widget.component.js
+++ src/app/pages/home/flight-search-widget/dist/flight-search-widget.component.js
@@ -260,9 +260,9 @@ var FlightSearchWidgetComponent = /** @class */ (function () {
         ;
         if (this.rangeDates[0] && this.rangeDates[1]) {
             var daysDiff = this.rangeDates[0] ? moment(this.rangeDates[1], "YYYY-MM-DD").diff(moment(this.rangeDates[0], "YYYY-MM-DD"), 'days') : 0;
-            this.returnDate = this.rangeDates[1];
             this.departureDate = this.rangeDates[0];
-            if (daysDiff == 0) {
+            this.returnDate = this.rangeDates[1];
+            if (!moment(this.rangeDates[1]).isAfter(moment(this.rangeDates[0]))) {
                 this.returnDate = moment(this.rangeDates[0]).add(7, 'days').toDate();
             }
             // this.flightDepartureMinDate = this.rangeDates[0];
--- src/app/pages/home/flight-search-widget/flight-search-widget.component.ts
+++ src/app/pages/home/flight-search-widget/flight-search-widget.component.ts
@@ -310,9 +310,10 @@ export class FlightSearchWidgetComponent implements OnInit {
     };
     if (this.rangeDates[0] && this.rangeDates[1]) {
       let daysDiff = this.rangeDates[0] ? moment(this.rangeDates[1], "YYYY-MM-DD").diff(moment(this.rangeDates[0], "YYYY-MM-DD"), 'days') : 0;
-      this.returnDate = this.rangeDates[1];
       this.departureDate = this.rangeDates[0];
-      if (daysDiff == 0) {
+      this.returnDate = this.rangeDates[1];
+      
+      if (!moment(this.rangeDates[1]).isAfter(moment(this.rangeDates[0]))) {
         this.returnDate = moment(this.rangeDates[0]).add(7, 'days').toDate();
       }
       // this.flightDepartureMinDate = this.rangeDates[0];
