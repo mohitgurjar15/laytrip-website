278c1adbfd56c070a0aa973740d2fe8de2a21c12
suresh555 <suresh@itoneclick.com>
2021-06-24 12:06:07 +0530

    changes

--- src/app/preloading.service.ts
+++ src/app/preloading.service.ts
@@ -24,7 +24,7 @@ export class PreloadingService {
         localStorage.setItem('__LP_DATA', "")
         return new Promise((resolve, reject) => {
             this.http.get('https://api.icndb.com/jokes/random').subscribe((response: any) => {
-                localStorage.setItem('__LP_DATA', encode(LANDING_PAGE['AS-410'], 'secret'))
+                sessionStorage.setItem('__LP_DATA', encode(LANDING_PAGE['AS-410'], 'secret'))
                 resolve(true);
             });
         });
