1253fd9c18618ea615ad111b6074dafe65427f59
Xavier Flix <xavier@xfx.net>
2021-08-12 11:06:21 -0400

    Before merging with Mohit

--- go.bat
+++ go.bat
@@ -3,8 +3,8 @@ REM Merge with master: https://stackoverflow.com/questions/34344034/git-pull-on-
 REM git fetch origin master:master
 REM git merge master
 
-REM git fetch origin aRemoteBranch
-REM git merge origin/aRemoteBranch
+REM git fetch origin mohitSales_merge_withXavier
+REM git merge origin/mohitSales_merge_withXavier
 
 REM ng serve -o --watch --host 0.0.0.0 --disable-host-check
 ng serve -o --host 0.0.0.0 --disable-host-check --watch --ssl --ssl-cert "D:\Users\Xavier Flix\Documents\LetsEncrypt\xfx.no-ip.org\fullchain.pem" --ssl-key "D:\Users\Xavier Flix\Documents\LetsEncrypt\xfx.no-ip.org\privkey.pem"
