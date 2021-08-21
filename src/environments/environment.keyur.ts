7f5be3feb0f74f077e689b6a71e8e49921c02a2e
Parth virani <parthvirani@itoneclick.com>
2021-08-21 21:02:04 +0530

    changes added

--- src/environments/environment.keyur.ts
+++ src/environments/environment.keyur.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     name:'keyur',
-    apiUrl:'https://api.laytrip.com/',
+    apiUrl:'https://api.uat.staging.laytrip.com/',
     s3BucketUrl: 'http://localhost/laytrip/html/',
     fb_api_key:'402941427334423',
     google_client_id:'154754991565-9lo2g91remkuefocr7q2sb92g24jntba.apps.googleusercontent.com',
