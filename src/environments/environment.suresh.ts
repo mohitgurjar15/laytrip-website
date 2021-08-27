2bfa09d05b3cdd5ecf04c66772acee218c45a46a
suresh555 <suresh@itoneclick.com>
2021-08-27 09:34:14 +0530

    changes

--- src/environments/environment.suresh.ts
+++ src/environments/environment.suresh.ts
@@ -3,7 +3,7 @@ export const environment = {
   name:'suresh',
   apiUrl:'http://localhost:4040/',
   //apiUrl:'https://api.uat.staging.laytrip.com/',
-  s3BucketUrl: 'http://localhost/laytrip/html/',
+  s3BucketUrl: 'http://localhost/laytrip-design-main/laytrip_ui/',
   fb_api_key:'402941427334423',
   google_client_id:'154754991565-9lo2g91remkuefocr7q2sb92g24jntba.apps.googleusercontent.com',
   siteUrl: 'https://alpha.laytrip.com',
