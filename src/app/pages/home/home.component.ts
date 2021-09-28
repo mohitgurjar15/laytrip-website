0d8800d9cecfe88824b846e35025bd6059b12737
Jaymees.Jaymees_Donga <Jaymees.jaymees@itoneclick.com>
2021-09-28 13:27:33 +0530

    changes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -73,8 +73,8 @@ export class HomeComponent implements OnInit {
     this.landingPageName = this.route.snapshot.queryParams['utm_source']
     this.tripfluencer = this.formBuilder.group({
       name: ['', [Validators.required,Validators.pattern('^(?! )(?!.* $)[a-zA-Z0-9,.: ()-]+$')]],
-      email: ['', [Validators.required,Validators.email,Validators.pattern('^(?! )(?!.* $)[a-zA-Z0-9,.: ()-]+$')]],
-      social_name: ['',Validators.pattern('^(?! )(?!.* $)[a-zA-Z0-9,.: ()-]+$')],
+      email: ['', [Validators.required,Validators.email]],
+      social_name: ['',Validators.pattern('^(?! )(?!.* $)[a-zA-Z0-9,_.: ()-]+$')],
     });
     this.host = window.location.host;
     this.isRefferal = this.commonFunction.isRefferal();
